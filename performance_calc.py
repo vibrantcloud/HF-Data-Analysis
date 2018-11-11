cd71ae9d18bf86efc9a960e98450ea32fcaa39f9
vibrantcloud <44446657+vibrantcloud@users.noreply.github.com>
2018-11-11 01:04:32 +0000

    Update performance_calc.py

--- performance_calc.py
+++ performance_calc.py
@@ -4,29 +4,30 @@ total_performance = 25
 minx = -20
 variable_hours = -18
 
-if total_sale > total_forecast and total_performance < 0:
+
+if total_sale > total_forecast and total_performance < 0: 
     print(0)
-elif total_performance >= 0:
+elif total_performance >= 0:                                
     print(total_performance)
-elif total_performance < minx and minx > variable_hours:
+elif total_performance < variable_hours and variable_hours > minx:
      print(variable_hours)
-elif total_performance < minx and minx < variable_hours:
+elif minx > total_performance and minx > variable_hours:
     print(minx)
 elif total_performance > minx and total_performance > variable_hours:
     print(total_performance) 
-elif total_performance > minx and total_performance < variable_hours:
-    print(total_performance)
+
+    
+
     
 ## Pandas Version ## 
 
 conditions = [(df['Total Sales'] > df['Total Forecast']) & (df['Total Perf'] < 0),
               (df['Total Perf'] >= 0),
-              (df['Total Perf'] < df['Min']) & (df['Min'] > df['Variable Hours']),
-              (df['Total Perf'] < df['Min']) & (df['Min'] < df['Variable Hours']),
-              (df['Total Perf'] > df['Min']) & (df['Total Perf'] > df['Variable Hours']),
-              (df['Total Perf'] > df['Min']) & (df['Total Perf'] < df['Variable Hours']) 
+              (df['Total Perf'] < df['Variable Hours']) & (df['Variable Hours'] > df['Min']),
+              (df['Min'] > df['Total Perf']) & (df['Min'] > df['Variable Hours']),
+              (df['Total Perf'] > df['Min']) & (df['Total Perf'] > df['Variable Hours'])
              ]
-outputs = [0,df['Total Perf'], df['Min'],df['Variable Hours'],df['Total Performance'],df['Total Performance']]
+outputs = [0,df['Total Perf'], df['Variable Hours'],df['Min'],df['Total Performance']]
     
     
     
