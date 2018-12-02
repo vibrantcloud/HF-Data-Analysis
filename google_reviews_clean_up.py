75c0b2c529c857e02839a9054fc9886956972678
vibrantcloud <44446657+vibrantcloud@users.noreply.github.com>
2018-12-02 20:57:14 +0000

    Update google_reviews_clean_up.py

--- google_reviews_clean_up.py
+++ google_reviews_clean_up.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 
 file = input(r"Enter your File Path : ")
-
 df = pd.read_csv(file +".csv")
 
 ## Melt the Curent DF ## 
@@ -12,7 +11,7 @@ new_df = pd.melt(df,id_vars=['ID'],var_name=['Store'],value_name='Score').fillna
 # sort index so all the 'Date' values are at the bottom
 new_df.sort_index(inplace=True) 
 
-# create a new df of just the dates becuase that is your review types
+# create a new df of just the dates because that is your review types
 review_types = new_df.loc['Date']
 
 # rename column to review types
@@ -26,5 +25,24 @@ new_df.rename(columns={'ID':'Date'}, inplace=True)
 
 # merge your two dataframes together
 new_df = new_df.merge(review_types, on='Store')
+new_df = new_df[['Store','Date','Score','Review Type']].copy()
 new_df['Store'] = new_df['Store'].astype(str).str.replace('.1','')
+new_df2 = new_df.loc[new_df.Score != 0].copy()
 
+
+new_df.to_csv('Cleaned_Reviews.csv',index=False)
+new_df2.to_csv('Cleaned_Reviews_NaNs_Removed.csv',index=False)
+
+""" 
+Unsure of Data so creating both variants of the code.
+
+"""
+
+
+df1.rename(columns={'Review Type' : 'Reviews'},inplace=True)
+df2.rename(columns={'Review Type' : 'Average Review'},inplace=True)
+df1.rename(columns={'Score' : 'Count'},inplace=True)
+df3 = pd.merge(df1,df2,on=['Date','Store'],how='left')
+df3 = df3[['Store','Date','Reviews','Count','Average Review','Score']].copy()
+
+df3.to_csv('Greviews.csv',index=False)
