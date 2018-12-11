84294f8fb4107b76fc593edc8fc7765bd1ceef7a
vibrantcloud <44446657+vibrantcloud@users.noreply.github.com>
2018-12-11 23:34:13 +0000

    Update Web_Scrape.py

--- Web_Scrape.py
+++ Web_Scrape.py
@@ -3,9 +3,15 @@ from requests import get
 import pandas as pd
 import numpy as np
 
-query = 
-search url = 'https://www.halfords.com/webapp/wcs/stores/servlet/SearchRouter?langId=-1&storeId=10001&catalogId=10151&action=search&srch=' + query
-response = get(url)
-print(response.text[:500])
 
-
+for x in df['Product ID']:
+    search_url = murl + x
+    print(search_url)
+    uclient = uReq(search_url)
+    print(uclient)
+    page_html = uclient.read()
+    uclient.close()
+    page_soup = soup(page_html,"html.parser")
+    rp = page_soup.findAll("h2",{"class" : 'highlightValue'})[0].text.strip()
+    
+    
