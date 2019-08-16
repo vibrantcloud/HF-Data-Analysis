77c3c049c19ebb9455538777794278bb26bd24fb
Umar-Hussain <44446657+vibrantcloud@users.noreply.github.com>
2019-08-16 04:11:43 +0100

    Update common_tasks.py

--- common_tasks.py
+++ common_tasks.py
@@ -2,14 +2,29 @@
 # do same for leavers. 
 
 # merge into one file. 
-
-columns = ['First Name', 'Last Name', 'Location Ledger Code', 'Job', 'Number',
-       'Normal Weekly Hours', 'Contract Type', 'Date Joined', 'Gender']
+columns = [
+    "First Name",
+    "Last Name",
+    "Location Ledger Code",
+    "Job",
+    "Number",
+    "Normal Weekly Hours",
+    "Contract Type",
+    "Date Joined",
+    "Gender",
+]
 
 col = col[columns].copy()
-col = pd.merge(col,dt,left_on='Date Joined',right_on='Date',how='left')
-col.rename(columns={'Location Ledger Code' : 'Store'},inplace=True)
-st = pd.read_sql("SELECT Shop AS Store, Area, Location, Division from StructureTab",engine)
-col = pd.merge(col,st,on='Store',how='left')
-col['Name'] = col['First Name'] + ' ' + col['Last Name']
-col = col.iloc[:,2:]
+
+col = pd.merge(col, dt, left_on="Date Joined", right_on="Date", how="left")
+
+col.rename(columns={"Location Ledger Code": "Store"}, inplace=True)
+
+st = pd.read_sql(
+    "SELECT Shop AS Store, Area, Location, Division from StructureTab", engine
+)
+col = pd.merge(col, st, on="Store", how="left")
+
+col["Name"] = col["First Name"] + " " + col["Last Name"]
+
+col = col.iloc[:, 2:]
