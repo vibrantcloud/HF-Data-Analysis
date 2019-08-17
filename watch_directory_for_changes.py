9ef11c211a9eb85f2989886511e28f03d0caa55b
Umar-Hussain <44446657+vibrantcloud@users.noreply.github.com>
2019-08-17 16:15:54 +0100

    Update watch_directory_for_changes.py

--- watch_directory_for_changes.py
+++ watch_directory_for_changes.py
@@ -1,15 +1,12 @@
 """
 This script will watch a folder for changes and do the following : 
 
-a) Rename the file according to the "Course Name" column in the csv.
+a) Rename the file according to the "Course Name" column in the csv or excel.
 b) If the file exists it will auto delete it and prompt a message telling the user the file existed.
 c) save about 15 minutes per week when doing this.
 
+will need to re-factor code to make it more "dry" lots of redundancy here.
 
-Things to add - 
-
-error handling for excel files (essentiall re-save as CSV to keep code clean)
-wsadmin report for apprenticeships. 
 """
 
 import time
@@ -28,18 +25,34 @@ print("Hi, this program will autorename the Training Credits files we receive on
 print(f"The current location is set to {my_path}")
 print("To close the program hit CTRL-C on your keyboard whilst on the command terminal.")
 
+
 os.chdir(my_path)
 
-# Create class.
+print("Hi, this program will autorename the Training Credits files we receive on a weekly basis")
+print(f"The current location is set to {my_path}")
+print("To close the program hit CTRL-C on your keyboard whilst on the command terminal.")
+file_date = datetime.today().strftime('%d%m%Y')
+
 class MyHandler(FileSystemEventHandler):
     def __init__(self):
         self.last_modified = datetime.now()
-        
-        
-#Create function that watches folder for Modifications. 
+
     def on_modified(self, event):
         if datetime.now() - self.last_modified < timedelta(seconds=1):
             return
+        elif event.src_path.split('.')[-1] == 'xlsx':
+            df = pd.read_excel(event.src_path) # read the file
+            course = df['Course Name'].unique().tolist()[0] # pass course name to a variable
+            try:
+                self.last_modified = datetime.now()
+                print(f'Event type: {event.event_type}  path : {event.src_path}')
+                print(course)
+                os.rename(event.src_path, f"{course+file_date}.xlsx") # rename file
+                print(f"file renamed to {course+file_date}.xlsx")
+            except (FileExistsError,FileNotFoundError):
+                print(f"This course file : {course+file_date} already exists.")
+                os.remove(event.src_path)
+                print("Duplicate file removed.")
         else:
             df = pd.read_csv(event.src_path) # read the file
             course = df['Course Name'].unique().tolist()[0] # pass course name to a variable
@@ -47,12 +60,13 @@ class MyHandler(FileSystemEventHandler):
                 self.last_modified = datetime.now()
                 print(f'Event type: {event.event_type}  path : {event.src_path}')
                 print(course)
-                os.rename(event.src_path, f"{course + file_date}.csv") # rename file
-                print(f"file renamed to {course + file_date}.csv")
+                os.rename(event.src_path, f"{course+file_date}.csv") # rename file
+                print(f"file renamed to {course+file_date}.csv")
             except (FileExistsError,FileNotFoundError):
-                print(f"This course file : {course + file_date} already exists.")
+                print(f"This course file : {course+file_date} already exists.")
                 os.remove(event.src_path)
-                print("Duplicate file removed")
+                print("Duplicate file removed.")
+    
                 
 
 # starts the program.
